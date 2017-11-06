196d6b9cefc7c70cf27d2be4d338910013a4ec74
Filip Lauc <filip.lauc93@gmail.com>
2017-11-06 22:06:14 +0100

    chore(index.ts): Added missing interface exports :hammer:

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import {NgModule} from '@angular/core';
 import {PushNotificationsService} from './services/push-notifications.service';
 
 export * from './services/push-notifications.service';
+export * from './interfaces/push-notification.type';
 
 @NgModule({
   providers: [PushNotificationsService],
